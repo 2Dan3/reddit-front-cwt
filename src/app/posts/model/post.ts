e9054015aa8d079c25115451964f15a728bc9f9b
2Dan3 <dan23.ftn@gmail.com>
2022-09-04 22:33:40 +0200

    Add: get posts in community api call

--- src/app/posts/model/post.ts
+++ src/app/posts/model/post.ts
@@ -6,11 +6,11 @@ export class Post {
     author_display_name: string;
 
     constructor(obj?: any) {
-        this._id = obj && obj._id || null;
+        this._id = obj && obj.id || null;
         this.title = obj && obj.title || null;
         this.text = obj && obj.text || null;
-        this.creation_date = obj && obj.creation_date || null;
-        this.author_display_name = obj && obj.author_display_name || null;
+        this.creation_date = obj && new Date(obj.creationDate).toLocaleDateString() || null;
+        this.author_display_name = obj && obj.author || null;
         // this._id = obj && obj._id || null;
     }
 
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api/api.service';
 import { Post } from './model/post';
 import { PostService } from './services/post.service';
 
@@ -16,13 +18,32 @@ export class PostsComponent implements OnInit {
 
   constructor(
     private postService : PostService,
+    private apiService : ApiService,
+    private route : ActivatedRoute,
     ) 
     {
-        console.log("from posts component, community_id: " +this.community_id);
+        console.log("from posts component, community_id: " + route.snapshot.params['id']);
+
+        this.apiService.get("/communities/"+ route.snapshot.params['id'] +"/posts")
+        .subscribe( 
+        (res) => {
+          this.posts = [];
+
+          for(let res_obj of res) {
+            this.posts.push(new Post(res_obj));
+          }
+          // console.log("response status;" + res['status']);
+          console.log("Result:" +JSON.stringify(res));
+          console.log("posts upon data arrival: ", this.posts);
+        },
+        (_error: any) => {
+          console.log("Error caught  " + _error);
+        });
     }
 
   ngOnInit(): void {
-    this.posts = this.postService.getAll(this.community_id);
+    // this.posts = this.postService.getAll(this.community_id);
+    // console.log("comm_id: " + this.community_id);
   }
 
   sortByDateDesc(){
@@ -50,6 +71,10 @@ export class PostsComponent implements OnInit {
   sortByTrendingAsc(){
     console.log("Sort By Trending: Coldest")
     // *TODO: Implement this.postService API call
+  }
+
+  protected isPostListEmpty(){
+    return this.posts.length == 0;
   }
   
 }
